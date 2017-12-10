c8f971726b5f2adb49a2fbb098a34379fa5ec40d
denovo <pete.richardson82@gmail.com>
2017-12-10 14:18:33 +0000

    more updates for VC

--- source/js/index.js
+++ source/js/index.js
@@ -6,7 +6,7 @@ var scroll = require('./modules/scroll');
 var dp = require('./modules/pixels');
 var StickyHeader = require('./modules/sticky-header');
 // var Instafeed = require("./modules/instafeed.min");
-var pixel_bg = new dp.DeadPixels();
+// var pixel_bg = new dp.DeadPixels();
 
 Blazy = require('./vendor/blazy.min.js');
 
@@ -33,7 +33,7 @@ window.bLazy = new Blazy({
 }
 );
 
-pixel_bg.initialize();
+// pixel_bg.initialize();
 
 
 // execute photoswipe function if there is a gallery on the page
@@ -48,3 +48,110 @@ if (document.querySelector('.my-gallery') !== null) {
 //     });
 //     feed.run();
 // }
+
+
+(function(win, doc, $){
+  
+  
+  win.colorSwarm = function(canvas) {
+  
+      var ctx;
+      var circles;
+      var tick = 0;
+      var numCircles = 100;
+      var h = eval(canvas.getAttribute('h') || '2/3');
+      
+    
+    // returns a random color from an array of possible colour values (colors array)
+      let randomColor = function() {
+        let colors = ['#FF5000', '#0078AE', '#F65097'];
+        return colors[Math.floor(Math.random() * colors.length)]
+      };
+    
+      var resize = window.resize = function() {
+          canvas.height = $(canvas).parent().outerHeight() * h;
+          canvas.width = window.innerWidth;
+      };
+  
+      $(function() {
+          ctx = canvas.getContext('2d');
+          resize();
+        
+          circles = [];
+  
+          for (var i=0; i<numCircles; i++) {
+              var x = Math.random()*canvas.width;
+              var y = Math.random()*canvas.height;
+              var c = new Circle(x, y, canvas.width, canvas.height);
+              c.draw();
+              circles.push(c);
+          }
+  
+          var loop = function() {
+              window.requestAnimFrame(loop);
+              ctx.clearRect(0, 0, canvas.width, canvas.height);
+              for (var i=0; i<circles.length; i++) {
+              circles[i].frame();
+              }
+          };
+  
+          window.requestAnimFrame = function(){
+              return window.requestAnimationFrame ||
+              window.webkitRequestAnimationFrame ||
+              window.mozRequestAnimationFrame ||
+              window.oRequestAnimationFrame ||
+              window.msRequestAnimationFrame ||
+              function(a) { window.setTimeout(a,1E3/60); };
+          }();
+  
+          loop();
+      });
+  
+    
+    
+  
+    
+      
+    
+      var Circle = function(x, y) {
+          this.pos = [ x, y ];
+          this.r = (1.5*Math.random())+0.2;
+          this.c = randomColor();
+          this.v = [
+              (Math.random()-0.5)*0.08,
+              (Math.random()-0.5)*0.08
+          ];
+      };
+  
+      Circle.prototype.getBound = function(i) {
+          return i ? canvas.height : canvas.width;
+      };
+  
+      var i;
+      Circle.prototype.frame = function() {
+          for (i=0; i<2; i++) {
+              if (this.pos[i] > this.getBound(i)-10) { this.v[i] *= -1; }
+              else if (this.pos[i] < 10) { this.v[i] *= -1; }
+              this.pos[i] += this.v[i]*10;
+          }
+  
+          this.draw();
+      };
+  
+      Circle.prototype.draw = function() {
+          ctx.fillStyle = this.c;
+          ctx.beginPath();
+          ctx.arc(this.pos[0], this.pos[1], this.r, 0, 2 * Math.PI, false);
+          ctx.fill();
+      };
+  }
+  
+  $(function() {
+      $('canvas[color-swarm]').each(function() {
+          colorSwarm(this);
+      });
+  })
+  
+  
+  
+  })(window, document, $);
